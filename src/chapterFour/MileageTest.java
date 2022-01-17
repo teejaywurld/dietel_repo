4cbf47ed70908ad8f77a2889273ebe62e7d88b03
Adetunji Henry <teejaywurld@gmail.com>
2022-01-17 05:19:09 +0100

    Update MileageTest.java

--- src/chapterFour/MileageTest.java
+++ src/chapterFour/MileageTest.java
@@ -1,10 +1,8 @@
 package chapterFour;
 
-    // Test application for class Gas
     public class MileageTest {
         public static void main( String args[] ) {
             Mileage application = new Mileage();
             application.calculateMPG();
-        } // end main
-    } // end class GasTest
-
+        }
+    }
